821b509fee4cb5e3f15ba3e33be3b1c5a86155f5
Kenan Franklin <kfranklin93@gmail.com>
2021-11-11 23:08:38 -0500

    finished header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <h1>Lets bu ild a tinder clone</h1>
+      {/* <h1>Lets build a tinder clone</h1> */}
       <Header />
-      {/* tinder cards */}
+      {/* tinder cards- REACT ROOTER - STOPS @ 48:23 on https://youtu.be/DQfeB_FKKkc */} 
       {/* buttos below tinder cards */}
 
       {/* chat screen */}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+// import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import IconButton from '@mui/material/IconButton';
 
 // functional component which returns some JSX 
 function Header() {
-    return(
-        <div>
-<h2>I am a header</h2>
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
+            <img
+                className="header__tinderLogo"
+                src="https://media.designrush.com/inspirations/129349/conversions/_1513706350_604_tin-preview.jpg"
+                alt="tinder logo" />
+            {/* <MeetingRoomIcon /> */}
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large" />
+            </IconButton>
         </div>
     )
 }
